7c88d2f1a8f2a1eaf302fa061f4d228dbb2a0873
Israel Manzo <israelmanzo814@gmail.com>
2019-02-02 18:48:12 -0500

    Added javsscript file

---
+++ JS/main.js
@@ -0,0 +1,50 @@
+// variables
+var colors = [];
+var pickedColor;
+var numberOfSquares = 6;
+var squares = document.querySelectorAll('.square');
+var colorDisplay = document.querySelector('#displayColor');
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+var resetBtn = document.querySelector('#reset');
+var modeBtn = document.querySelectorAll('.mode');
+
+// initalizer function
+
+function init(){
+
+}
+
+function setupModeButtons(){
+
+}
+
+function setupSquares(){
+
+}
+
+function reset(){
+
+}
+
+function changeColors(color){
+
+}
+
+function pickColor(){
+
+}
+
+function generateRandomColors(number){
+
+}
+
+// Function creates random color
+function randomColor(){
+    // red 0 -255
+    var red = Math.floor(Math.random() * 256);
+    // green 0 -255
+    var green = Math.floor(Math.random() * 256);
+    // blue 0 -255
+    var blue = Math.floor(Math.random() * 256);
+}
